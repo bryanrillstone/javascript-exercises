617304a81749d01f27fc801d9221de31fe11d6d0
Bryan Rillstone <bryan.rillstone@gmail.com>
2022-05-23 19:29:47 -0400

    uplading my solution

---
+++ lucky-numbers/script.js
@@ -0,0 +1,22 @@
+// Exercism - Lucky Numbers
+
+// Task 1 - Calculate the sum for the numbers on the slot machine
+
+function twoSum(array1, array2) {
+    return Number(array1.join('')) + Number(array2.join(''));
+  }
+
+// Task 2 - Determine if a number is a palindrome
+
+function luckyNumber(value) {
+    return value == String(value).split('').reverse().join('');
+  }
+
+// Task 3 - Generate an error message for invalid user input
+
+function errorMessage(input) {
+    if ((input == '') || (input === null) || (input === undefined)) {
+      return 'Required field'
+    } 
+    return ((isNaN(input)) || (input == 0)) ? 'Must be a number besides 0' : '';
+  }
